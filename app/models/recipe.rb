5cefcb7da2aad525b609057fcfaa06600ccb9ca7
Salil Chincholikar <chincholikarsalil@gmail.com>
2021-11-23 10:25:31 +0000

    add 1-m* association between recipe and chef

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   validates :name, presence: :true, length: {minimum: 5, maximum: 100}
   validates :description, presence: :true, length: {minimum: 10, maximum: 500}
+  
+  belongs_to :chef
+  validates :chef_id, presence: :true
 end
